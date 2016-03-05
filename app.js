1a2bba45262e5f78a404630c8ad31d2554661075
inmank <inmank@users.noreply.github.com>
2016-03-05 11:38:02 -0500

    Added files via upload

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
 var router = express.Router();
 app.use('/', router);
 app.use(express.static(__dirname + '/app'));
